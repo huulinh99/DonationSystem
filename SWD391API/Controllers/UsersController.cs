32d5ed4a0c86c340d19b1ed5bdab8ec0c29a1c06
linhnhse130563 <linhnhse130563@fpt.edu.vn>
2020-06-10 18:14:02 +0700

    asdas

--- SWD391API/Controllers/UsersController.cs
+++ SWD391API/Controllers/UsersController.cs
@@ -27,8 +27,8 @@ namespace SWD391API.Controllers
         {
 
             var user = _context.Users
-                                     .FromSqlRaw("Select COUNT(c.UserId) as totalCampaigns, (SELECT Count(ca.CampaignId) from Carelesses ca where ca.UserId=(select top 1 UserId From Carelesses Group by UserId Order by COUNT(CampaignId) Desc)) as totalLikes from Users u join Campaigns c on u.UserId= c.UserId where u.UserId=( select top 1 UserId From Carelesses Group by UserId Order by COUNT(CampaignId) Desc)")
-                                     .ToList();
+                .FromSqlRaw("Select COUNT(c.UserId) as totalCampaigns, (SELECT Count(ca.CampaignId) from Carelesses ca where ca.UserId=(select top 1 UserId From Carelesses Group by UserId Order by COUNT(CampaignId) Desc)) as totalLikes from Users u join Campaigns c on u.UserId= c.UserId where u.UserId=( select top 1 UserId From Carelesses Group by UserId Order by COUNT(CampaignId) Desc)")
+                .ToList();
             
             return Ok(user);
         }
@@ -38,7 +38,7 @@ namespace SWD391API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Users>>> GetUsers(string userId)
         {
-            var users =  _context.Users.Fr        
+            var users =  _context.Users.Where(u=>u.UserId.Equals(userId)).ToList();         
             return users;
         }
 
